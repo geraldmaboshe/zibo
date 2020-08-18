bc81e70b911509b3c7815459d9b344d4d31e1616
Gerald Maboshe <gerald@thezig.io>
2020-08-18 07:54:08 +0200

    removed comments

--- apis/Apartments.js
+++ apis/Apartments.js
@@ -11,12 +11,11 @@ const storage = multer.diskStorage({
 const upload = multer({ storage: storage });
 
 const Apartment = require('../models/Apartment');
-//get all apartments
+
 router.get('/', (req, res) => {
   Apartment.find().then(apartments => res.json(apartments));
 });
 
-//add apartment
 router.post('/', upload.single('apartmentImage'), (req, res) => {
   const newApartment = Apartment({
     title: req.body.title,
@@ -27,7 +26,7 @@ router.post('/', upload.single('apartmentImage'), (req, res) => {
   });
   newApartment.save().then(apartment => res.json(apartment));
 });
-//update
+
 router.put('/:id', upload.single('apartmentImage'), (req, res) => {
   const id = req.params.id;
   const body = req.body;
@@ -53,13 +52,13 @@ router.put('/:id', upload.single('apartmentImage'), (req, res) => {
     .then(() => res.json({ success: true }))
     .catch(error => res.status(404).json({ success: false }));
 });
-// get apartment by id
+
 router.get('/:id', (req, res) => {
   Apartment.findById(req.params.id)
     .then(apartment => res.json(apartment))
     .catch(error => res.status(404).json({ success: false }));
 });
-//delete apartment
+
 router.delete('/:id', (req, res) => {
   Apartment.findById(req.params.id)
     .then(apartment =>
