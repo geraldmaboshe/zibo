0ded62f69dc8d49708625e07856e0438a5bdb6fd
Gerald Maboshe <gerald@thezig.io>
2020-08-19 07:49:11 +0200

    added email form

--- client/src/components/ApartmentDetail.js
+++ client/src/components/ApartmentDetail.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { Row, Col, Container } from 'reactstrap';
+import { Button, Form, FormGroup, Label, Input } from 'reactstrap';
 
 export default class apartmentDetail extends Component {
   constructor(props) {
@@ -8,6 +9,7 @@ export default class apartmentDetail extends Component {
       apartmentId: null,
       apartmentDetail: null
     };
+    this.handleSubmit = this.handleSubmit.bind(this);
   }
   componentDidMount() {
     const apartmentId = this.props.match.params.id;
@@ -16,27 +18,50 @@ export default class apartmentDetail extends Component {
       .then(res => res.json())
       .then(data => this.setState({ apartmentDetail: data }));
   }
+  handleSubmit(e) {
+    e.preventDefault();
+  }
+
   render() {
     const { apartmentDetail } = this.state;
     return (
       <Container fluid>
-        <Row>
-          <Col>
-            <img
-              src={`https://zibo.herokuapp.com/${apartmentDetail?.apartmentImage}`}
-              alt={apartmentDetail?.title}
-              width="200px"
-              height="150px"
-            />
-          </Col>
-          <Col>
-            <div>{apartmentDetail?.title}</div>
-            <div>{apartmentDetail?.Specification}</div>
-            <div>{apartmentDetail?.location}</div>
-            <div>{apartmentDetail?.amount}</div>
-            <button>Checkout</button>
-          </Col>
-        </Row>
+        <div className="details-container">
+          <Row>
+            <Col xs={6}>
+              <img
+                src={`https://zibo.herokuapp.com/${apartmentDetail?.apartmentImage}`}
+                alt={apartmentDetail?.title}
+                width="600px"
+              />
+            </Col>
+            <Col xs={6}>
+              <p className="more-info">{apartmentDetail?.title}</p>
+              <p className="more-info">{apartmentDetail?.Specification}</p>
+              <p className="more-info">{apartmentDetail?.location}</p>
+              <p className="more-info">{apartmentDetail?.amount}</p>
+              <Form>
+                <p className="request-tour">Request Tour</p>
+                <FormGroup>
+                  <Label for="exampleEmail" hidden>
+                    Email
+                  </Label>
+                  <Input
+                    type="email"
+                    name="email"
+                    placeholder="Enter your Email here"
+                    width="50px"
+                  />
+                </FormGroup>
+                <div className="submit">
+                  <Button color="primary" onClick={this.handleSubmit}>
+                    Submit
+                  </Button>
+                </div>
+              </Form>
+            </Col>
+          </Row>
+        </div>
       </Container>
     );
   }
